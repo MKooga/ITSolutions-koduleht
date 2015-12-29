712982f04ecadb78dc1fd732e860cc7550340b44
Rajur <rajur.hint@gmail.com>
2015-12-30 01:53:17 +0200

    Database createad and syncronized

--- img_test.php
+++ img_test.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Rajur
- * Date: 30-12-15
- * Time: 01:51
- */
+// Connect to database server
+mysql_connect("mysql.myhost.com", "user", "sesame") or die (mysql_error ());
+
+// Select database
+mysql_select_db("mydatabase") or die(mysql_error());
+
+// SQL query
+$strSQL = "SELECT * FROM people";
+
+// Execute the query (the recordset $rs contains the result)
+$rs = mysql_query($strSQL);
+
+// Loop the recordset $rs
+// Each row will be made into an array ($row) using mysql_fetch_array
+while($row = mysql_fetch_array($rs)) {
+
+    // Write the value of the column FirstName (which is now in the array $row)
+    echo $row['FirstName'] . "<br />";
+
+}
+
+// Close the database connection
+mysql_close();
